166d7b34c7796627dd51f37f8a2e3854e40a07af
Vilberto02 <albertojulcapatricio@gmail.com>
2024-08-10 23:44:00 -0500

    feat: initialize new project in Next.js for a inventory app

---
+++ inventario-frontend/src/app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <main className={styles.main}>Hola mundo</main>;
+}
