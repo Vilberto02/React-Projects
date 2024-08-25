3247eedf6b2d6804a004c1d46181a2ca24a94698
Vilberto02 <albertojulcapatricio@gmail.com>
2024-08-25 02:32:52 -0500

    Añadir archivo con el objeto que contiene la lista de notas

---
+++ Javascript-to-React/note-app/src/data/notes.js
@@ -0,0 +1,44 @@
+export const list_notes = [
+  {
+    id: 0,
+    title: "Titulo 01",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, est numquam? Dolorum facere quisquam quia quae quaerat cum sed placeat, iusto explicabo, unde cumque fugit nesciunt blanditiis, ipsa necessitatibus quis.",
+    date: "10/02/2024",
+  },
+  {
+    id: 1,
+    title: "Titulo 02",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, est numquam? Dolorum facere quisquam quia quae quaerat cum sed placeat, iusto explicabo, unde cumque fugit nesciunt blanditiis, ipsa necessitatibus quis.",
+    date: "15/04/2024",
+  },
+  {
+    id: 2,
+    title: "Titulo 03",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, est numquam? Dolorum facere quisquam quia quae quaerat cum sed placeat, iusto explicabo, unde cumque fugit nesciunt blanditiis, ipsa necessitatibus quis.",
+    date: "18/05/2024",
+  },
+  {
+    id: 3,
+    title: "Titulo 04",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, est numquam? Dolorum facere quisquam quia quae quaerat cum sed placeat, iusto explicabo, unde cumque fugit nesciunt blanditiis, ipsa necessitatibus quis.",
+    date: "21/05/2024",
+  },
+  {
+    id: 4,
+    title: "Titulo 05",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, est numquam? Dolorum facere quisquam quia quae quaerat cum sed placeat, iusto explicabo, unde cumque fugit nesciunt blanditiis, ipsa necessitatibus quis.",
+    date: "29/06/2024",
+  },
+  {
+    id: 5,
+    title: "Titulo 06",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, est numquam? Dolorum facere quisquam quia quae quaerat cum sed placeat, iusto explicabo, unde cumque fugit nesciunt blanditiis, ipsa necessitatibus quis.",
+    date: "16/07/2024",
+  },
+];
